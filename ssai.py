07c1a137006b20f41d26771c3a60ced957cd915b
yogivardhanpuppala009-lgtm <yogivardhanpuppala009@gmail.com>
2026-08-31 15:54:57 +0530

    comparison operators

---
+++ ssai.py
@@ -0,0 +1,20 @@
+a=96
+b=74
+result= a==b
+print("result of ",a,"==",b,"is:",result)
+
+result= a!=b
+print("result of ",a,"!=",b,"is:",result)
+
+result= a>b
+print("result of ",a,">",b,"is:",result)
+
+result= a<b
+print("result of ",a,"<",b,"is:",result)
+
+result= a>=b
+print("result of ",a,">=",b,"is:",result)
+
+result= a<=b
+print("result of ",a,"<=",b,"is:",result)
+
