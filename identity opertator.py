a8e34ac76d3a204a3f4d830e4e5908af4b0acea2
yogivardhanpuppala009-lgtm <yogivardhanpuppala009@gmail.com>
2026-09-04 14:23:44 +0530

    identity oparator

---
+++ identity opertator.py
@@ -0,0 +1,9 @@
+a=[6,7,8]
+b=a
+result= b is a #is identity operator
+print("result of",b,"is",a,"is",result)
+
+c=[1,9,3]
+d=[1,8,3]
+result= c is d #is identity operator
+print("result of",c,"is",d,"is",result)
