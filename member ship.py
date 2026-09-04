c5d7130bbf1c00f08aea31f8015ab8fa5a125053
yogivardhanpuppala009-lgtm <yogivardhanpuppala009@gmail.com>
2026-09-04 14:21:42 +0530

    member ship
    membership

---
+++ member ship.py
@@ -0,0 +1,13 @@
+#member ship 
+a=[88,99,11]
+result=88 in a 
+print("result of 88 in",a,"is",result)
+
+result=33 not in a 
+print("result of 33 not in",a,"is", result)
+
+result=99 not in a 
+print ("result of 99 not in",a, "is",result)
+
+result=88 not in a
+print("result of 88 not in",a,"is",result )
