48b9af79213611b73c976654f75c106d42435da1
yogivardhanpuppala009-lgtm <yogivardhanpuppala009@gmail.com>
2026-09-15 13:58:30 +0530

    #lab task -11

---
+++ #lab task-11 comparison.py
@@ -0,0 +1,12 @@
+#Python Comparison Exercise
+a =89
+b =-90
+result = a > b #Greater than comparison Exercise
+print("result of",a, ">",b,"is",result)
+
+result = a < b #Less than comparison Exercise
+print("result of",a, "<",b,"is",result)
+result = a >= b #Greater than or equal to comparison Exercise
+print("result of",a, ">=",b,"is",result)
+result = a <= b #Less than or equal to comparison Exercise
+print("result of",a, "<=",b,"is",result)
