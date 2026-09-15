67af07e2bc66e2f33c0917d4c917d585ee6b15b7
yogivardhanpuppala009-lgtm <yogivardhanpuppala009@gmail.com>
2026-09-15 13:16:57 +0530

    lab task-6

---
+++ #task-6.py
@@ -0,0 +1,5 @@
+p = float(input("enter the principle amount" ))
+r = float(input("enter the rate of interest"))
+t = float(input("enetr the time period "))
+SI = (p* r* t) /100
+print("the  simple intrest is:",SI)
