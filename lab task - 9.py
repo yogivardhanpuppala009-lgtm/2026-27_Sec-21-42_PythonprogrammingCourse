aa3ff12f5e2cad1ea5d6a73d8174d6cb669c7d4a
yogivardhanpuppala009-lgtm <yogivardhanpuppala009@gmail.com>
2026-09-15 13:40:50 +0530

    lab task -9

---
+++ lab task - 9.py
@@ -0,0 +1,28 @@
+#Arithmetic Operators Exercise
+a=2
+b=5
+print("Addition of a and b is:", a+b)
+print("subtraction of a and b is:", a-b)
+print("Multiplication of a and b is:", a*b)
+print("Division of a and b is:", a/b)
+print("Modulus of a and b is:", a%b)
+print("Exponent of a and b is:", a**b)
+
+
+a=-10
+b=-90
+print("Addition of a and b is:", a+b)
+print("subtraction of a and b is:", a-b)
+print("Multiplication of a and b is:", a*b)
+print("Division of a and b is:", a/b)
+print("Modulus of a and b is:", a%b)
+print("Exponent of a and b is:", a**b)
+
+a=90
+b=89
+print("Addition of a and b is:", a+b)
+print("subtraction of a and b is:", a-b)
+print("Multiplication of a and b is:", a*b)
+print("Division of a and b is:", a/b)
+print("Modulus of a and b is:", a%b)
+print("Exponent of a and b is:", a**b)
